dc3713b0d802bdb143b4caaf2148af0d5b333d14
gabrielgua <gabrieljmym13@gmail.com>
2026-07-03 14:38:09 -0300

    chore: add tailwindcss config

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+  <p class="text-lg font-bold">
+    Bem-vindo ao WEX CLEAN!
   </p>
 </template>
 
-<style scoped></style>
+<style></style>
